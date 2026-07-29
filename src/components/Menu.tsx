77e5fc06b13eaed2e6e95b9868c5fd7df845ec5d
devgveb <gabrielbelodesenvolvedor@gmail.com>
2026-07-28 22:57:37 -0300

    Refactor Menu component layout and classes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,13 @@
 const Menu = () =>{
     return(
         <nav className="w-full">
-            <div className="container mx-auto flex items-center justify-between border-bottom">
+            <div className="mainContainer mx-auto flex items-center flex-row flex-wrap justify-between border-bottom">
                 <div className="pt-5 pb-5">
                     <a>Gabriel Belo</a>
                     <p>Analista de TI | Desenvolvedor</p>
                 </div>
 
-                <nav className="pl-5 pr-5 flex">
+                <nav className="pl-5 pr-5 hidden lg:flex ">
                     <ul className="flex items-center pt-5 pb-5 ">
                         <li className="pr-10"><a>Início</a></li>
                         <li className="pr-10"><a>Sobre Min</a></li>
